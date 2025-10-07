5ac166d5a72e1db513c3eeb77ceda86a2a40e0e9
student-pro <student-pro@b206-6.khpk.ru>
2025-10-07 16:23:33 +0700

    Done gawddaymn

--- app/src/main/java/com/example/login_profile/MainActivity2.kt
+++ app/src/main/java/com/example/login_profile/MainActivity2.kt
@@ -1,26 +1,20 @@
 package com.example.login_profile
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main2)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        val intent: Intent = Intent(this, MainActivity2::class.java)
-        findViewById<Button>(R.id.loginButton).setOnClickListener {
-            startActivity(intent)
         }
     }
 }
